55e8c790323a02e035c654ecc7197dd70a52e130
xushuhui <474497097@qq.com>
2020-08-26 10:27:30 +0800

    leetcode

---
+++ editor/cn/[102]二叉树的层序遍历.go
@@ -0,0 +1,42 @@
+package leetcode
+
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例： 
+//二叉树：[3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 612 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	list := make([][]int, 0)
+	return list
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/structure.go
@@ -0,0 +1,11 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
