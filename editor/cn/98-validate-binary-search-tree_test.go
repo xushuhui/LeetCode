e7a418e0f487e84d6151038e4ee896bd1bf1c2dc
徐曙辉 <xushuhui@himango.cn>
2021-08-20 09:28:55 +0800

    cn convert en

---
+++ editor/cn/98-validate-binary-search-tree_test.go
@@ -0,0 +1,56 @@
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+//
+// 假设一个二叉搜索树具有如下特征：
+//
+//
+// 节点的左子树只包含小于当前节点的数。
+// 节点的右子树只包含大于当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+//
+//
+// 示例 1:
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+//
+//
+// 示例 2:
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树
+// 👍 1173 👎 0
+
+package leetcode
+
+import "testing"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func Test_validateBinarySearchTree(t *testing.T) {
+
+}
