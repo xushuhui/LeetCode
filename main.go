53b234d2b8e3a15e23e1d71e727daf5ee7e8911d
徐曙辉 <xushuhui@himango.cn>
2021-08-23 18:12:52 +0800

    283

--- main.go
+++ main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
+type TestHandler struct {
+	str string
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("HandleFunc")
+	w.Write([]byte(string("HandleFunc")))
+	go func() {
+		fmt.Println("start")
+		time.Sleep(5 * time.Second)
+		fmt.Println("end")
+	}()
+}
+
+//ServeHTTP方法，绑定TestHandler
+func (th *TestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Handle")
+	w.Write([]byte(string("Handle")))
+}
+
 func main() {
-	defer_call()
+	http.Handle("/", &TestHandler{"Hi"}) //根路由
+	http.HandleFunc("/t", SayHello)      //test路由
+	http.ListenAndServe("127.0.0.1:8000", nil)
 }
 
 func defer_call() {
