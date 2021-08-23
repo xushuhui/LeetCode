194b2b8a282409a50c8375b0b12cb89f395a4b99
徐曙辉 <xushuhui@himango.cn>
2021-08-23 18:26:21 +0800

    question

---
+++ editor/cn/145-binary-tree-postorder-traversal_test.go
@@ -0,0 +1,39 @@
+//给定一个二叉树，返回它的 后序 遍历。
+//
+// 示例:
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [3,2,1]
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 深度优先搜索 二叉树
+// 👍 655 👎 0
+
+package leetcode
+
+import "testing"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func Test_binaryTreePostorderTraversal(t *testing.T) {
+
+}
